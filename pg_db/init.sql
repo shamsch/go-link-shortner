32e4b435b5fa49d74f0e609c092a393c9e96d674
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2022-12-27 20:10:36 +0200

    init script fixed

--- pg_db/init.sql
+++ pg_db/init.sql
@@ -1,8 +1,7 @@
-CREATE TABLE IF NOT EXISTS `link` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(255) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `shortUrl` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-)
+CREATE TABLE IF NOT EXISTS link (
+  id SERIAL PRIMARY KEY,
+  longUrl varchar(255) NOT NULL,
+  title varchar(255) NOT NULL,
+  createdAt timestamp NOT NULL DEFAULT NOW(),
+  shortUrl varchar(255) NOT NULL
+);
