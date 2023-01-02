643854bce92aade9208332b0354db83aabd24395
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2023-01-02 21:05:11 +0200

    init server

---
+++ backend/model/server.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type Link struct {
+	Id int    `json:"id" gorm:"primaryKey"`
+	Url string `json:"url"`
+	ShortUrl string `json:"shortUrl"`
+}
+
+func InitDB() {
+	var err error
+	// NEEDS TO BE CHANGED TO POSTGRES RUNNING ON DOCKER COMPOSE NETWORK
+	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err = gorm.Open(
+		postgres.New(postgres.Config{
+			DSN:                  dsn,
+			PreferSimpleProtocol: true,
+		}),
+		&gorm.Config{},
+	)
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+}
