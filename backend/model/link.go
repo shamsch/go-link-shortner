a48d667ba036ccc75a12dcc0a42facb0ae81c9c0
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2023-01-03 22:34:57 +0200

    primary key changed

--- backend/model/link.go
+++ backend/model/link.go
@@ -2,6 +2,6 @@ package model
 
 type Link struct {
 	Id int    `json:"id" gorm:"autoIncrement"`
-	Url string `json:"url" gorm:"primaryKey;unique;not null"`
-	ShortUrl string `json:"shortUrl"`
+	Url string `json:"url" gorm:"not null"`
+	ShortUrl string `json:"shortUrl" gorm:"primaryKey;unique;not null"`
 }
