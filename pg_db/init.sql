6aa30afce19ec768747744e57821a50948a4c680
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2023-01-05 23:01:09 +0200

    init sql empty

--- pg_db/init.sql
+++ pg_db/init.sql
@@ -1,7 +1,1 @@
-CREATE TABLE IF NOT EXISTS link (
-  id SERIAL PRIMARY KEY,
-  longUrl varchar(255) NOT NULL,
-  title varchar(255) NOT NULL,
-  createdAt timestamp NOT NULL DEFAULT NOW(),
-  shortUrl varchar(255) NOT NULL
-);
+ 
