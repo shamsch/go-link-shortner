b3e354602bc186d58e7e483ec2f7f017db1a8967
Shamsur Raza Chowdhury <shamsur314@gmail.com>
2023-01-06 19:59:40 +0200

    make sure that the shortUrl is unique

--- backend/service/create.go
+++ backend/service/create.go
@@ -10,7 +10,9 @@ import (
 func CreateShortenUrl(url string) string {
 	var existingUrl model.Link
 
-	db.DB.First(existingUrl, "url = ?", url)
+	fmt.Println("Checking if this link already exists in the database")
+
+	db.DB.First(&existingUrl, "url = ?", url)
 	
 	if existingUrl.Url == url {
 		fmt.Println("Link already exists")
@@ -18,6 +20,19 @@ func CreateShortenUrl(url string) string {
 	}
 	
 	shortenUrl := util.RandString(8)
+
+	// run a loop to check if the shortenUrl already exists
+
+	for {
+		var existingShortUrl model.Link
+		db.DB.First(&existingShortUrl, "short_url = ?", shortenUrl)
+		if existingShortUrl.ShortUrl == shortenUrl {
+			shortenUrl = util.RandString(8)
+		} else {
+			break
+		}
+	}
+
 	link := model.Link{
 		Url: url,
 		ShortUrl: shortenUrl,
